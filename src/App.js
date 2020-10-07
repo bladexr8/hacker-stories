7ae721aae54afce407282ba52d382249708f2af5
bladexr8 <bladexr8@gmail.com>
2020-10-07 22:35:07 +1100

    Created Custom React Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// custom hook
+const useSemiPersistentState = (key, initialState) => {
+  // useState React hook, uses initial state as an argument
+  // searchTerm = current state
+  // setSearchTerm = function to update state
+  // array destructuring used to populate with return values from useState
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  // React useEffect hook
+  // first argument is function to be called every time searchTerm changes
+  // second argument is dependency array of variables
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+
+}
+
+// Main App Component
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+  ]
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(story => 
+    story.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search search={searchTerm} onSearch={handleSearch} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+
+      <hr />
+
+      <List list={searchedStories} />
+      
     </div>
   );
-}
+};
+
+// Search Component
+const Search = ({ search, onSearch}) => (
+  
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={search} onChange={onSearch} />
+    </div>
+
+);
+
+// List Component
+const List = ({ list }) => 
+  list.map(item => <Item key={item.objectID} item={item} />);
+      
+// Item component
+const Item = ({ item }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title} </a>
+    </span>
+    <span>{item.author} </span>
+    <span>{item.num_comments} </span>
+    <span>{item.points} </span>
+  </div>
+);
 
 export default App;
