6ba072ad604b1fa74cf0b0440b1bf1df363e15ad
bladexr8 <bladexr8@gmail.com>
2020-10-08 11:30:19 +1100

    finished inline JSX handler chapter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
+// initial list of stories
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: 'Nodejs',
+    url: 'https://nodejs.org',
+    author: 'Ryan Dahl',
+    num_comments: 3,
+    points: 10,
+    objectID: 2
+  },
+]
+
 // custom hook
 const useSemiPersistentState = (key, initialState) => {
   // useState React hook, uses initial state as an argument
@@ -24,26 +52,18 @@ const useSemiPersistentState = (key, initialState) => {
 // Main App Component
 const App = () => {
 
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1
-    },
-  ]
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  // load state for stories
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  }
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -59,7 +79,14 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel 
+        id="search"
+        value={searchTerm} 
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <p>
         Searching for <strong>{searchTerm}</strong>
@@ -67,28 +94,66 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
       
     </div>
   );
 };
 
 // Search Component
-const Search = ({ search, onSearch}) => (
-  
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" value={search} onChange={onSearch} />
-    </div>
+// A - create a "ref" with React's useRef hook. This contains a mutable "current" property.
+//     "ref" itself is a persistent value which stays intact over lifetime of React component
+// B - ref is passed to input field's JSX reserved "ref" attribute and the element
+//     instance is assigned to the changeable "current" property
+// C - opt into React's lifecycle with "useEffect" hook, performing the focus on the input
+//     field when the component renders
+// D - since ref is passed to the input field's "ref" attribute, it's current property
+//     gives access to the element. Execute focus programmatically as a side-effect, but only
+//     if "isFocused" is set and "current" property is existent
+const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, children}) => {
 
-);
+    // A
+    const inputRef = React.useRef();
+
+    // C
+    React.useEffect(() => {
+      if (isFocused && inputRef.current) {
+        // D
+        inputRef.current.focus()
+      }
+    }, [isFocused]);
+  
+    return (
+      <>
+      {/* React Fragment */}
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      {/* B */}
+      <input
+        ref={inputRef} 
+        id={id} 
+        type={type} 
+        value={value} 
+        autoFocus={isFocused} 
+        onChange={onInputChange} 
+      />
+      </>
+    );
+
+};
 
 // List Component
-const List = ({ list }) => 
-  list.map(item => <Item key={item.objectID} item={item} />);
+const List = ({ list, onRemoveItem }) => 
+  list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />);
       
 // Item component
-const Item = ({ item }) => (
+const Item = ({ item, onRemoveItem }) => {
+  
+  // remove an item from list
+  const handleRemoveItem = () => 
+    onRemoveItem(item);
+
+  return (
   <div>
     <span>
       <a href={item.url}>{item.title} </a>
@@ -96,7 +161,13 @@ const Item = ({ item }) => (
     <span>{item.author} </span>
     <span>{item.num_comments} </span>
     <span>{item.points} </span>
+    <span>
+      <button type="button" onClick={handleRemoveItem}>
+        Dismiss
+      </button>
+    </span>
   </div>
-);
+  );
+};
 
 export default App;
