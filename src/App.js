eb14081d752885a447fbbc66c35cee7a3be1aa33
bladexr8 <bladexr8@gmail.com>
2020-10-08 19:22:15 +1100

    Implement axios for data fetching

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 // API Endpoint
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
@@ -91,12 +92,12 @@ const App = () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT'});
 
     // get data from API
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits
+          payload: result.data.hits
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
